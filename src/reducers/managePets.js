67acc1e3544adaa88604311901b4c9e8a214d3e2
Tom Tobar <tomas.tobar@flatironschool.com>
2021-06-19 04:43:57 +0000

    Automatically backed up by Learn

--- src/reducers/managePets.js
+++ src/reducers/managePets.js
@@ -2,23 +2,16 @@ export let state;
 
 export function managePets(state = { pets:[] }, action){
   switch(action.type){
+    
     case "ADD_PET":
-      //make a copy of the pets array to not mutate original state.
       const petsCopy = [...state.pets];
-      
-      //push the new pet into the pets copy array.
       petsCopy.push(action.pet)
-      
-      // return the new state with the pets key set to the pets copy 
       return { pets: petsCopy }
+      
     case "REMOVE_PET":
-      //make a copy of the pets array to not mutate original state.
-      // find index of pet using indexOf
-      // save pet object to variable 
-      // Splice out the pet object from pets copy 
-      // return petsCopy
-      
-      
+      const petsCopy = [...state.pets];
+      const newPets = petsCopy.filter(pet => pet.id !== action.pet.id)
+      return { pets: newPets }
       
     default: 
       return state
